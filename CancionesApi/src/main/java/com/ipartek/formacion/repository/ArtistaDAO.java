5ab767a4ccb6a50b5a19340e5e5116f79352ea6e
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-11-23 17:10:45 +0100

    .

---
+++ CancionesApi/src/main/java/com/ipartek/formacion/repository/ArtistaDAO.java
@@ -0,0 +1,5 @@
+package com.ipartek.formacion.repository;
+
+public class ArtistaDAO {
+
+}
---
+++ CancionesApi/src/main/java/com/ipartek/formacion/repository/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrador
+ *
+ */
+package com.ipartek.formacion.repository;
